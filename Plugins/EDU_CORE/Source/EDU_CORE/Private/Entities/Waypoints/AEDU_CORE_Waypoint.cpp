66cb18b3b76b9958d1207478422e4081f454122d
GitDraug <Draug+github@outlook.com>
2025-01-25 16:28:40 +0100

    Rewrite

---
+++ Plugins/EDU_CORE/Source/EDU_CORE/Private/Entities/Waypoints/AEDU_CORE_Waypoint.cpp
@@ -0,0 +1,174 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EDU_CORE/Public/Entities/Waypoints/EDU_CORE_Waypoint.h"
+#include "Interfaces/EDU_CORE_CommandInterface.h"
+#include "Framework/Data/FLOWLOGS/FLOWLOG_AI.h"
+#include "Framework/Managers/GameModes/EDU_CORE_GameMode.h"
+
+#include "Net/UnrealNetwork.h"
+
+//------------------------------------------------------------------------------
+// Construction & Init
+//------------------------------------------------------------------------------
+AEDU_CORE_Waypoint::AEDU_CORE_Waypoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{ FLOW_LOG
+
+	// Disable ticking
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// We don't do normal Selection with Waypoints.
+	bCanBeRectangleSelected = false;
+	
+	// Enable replication
+	SetReplicates(true);
+	SetNetUpdateFrequency(10);
+}
+
+void AEDU_CORE_Waypoint::InitiateWaypoint(const EEDU_CORE_Team Team, const FVector& WorldLocation, const FRotator& WorldRotation)
+{
+	// Give it an ID that works across instances
+	SetWaypointID(FGuid::NewGuid());
+
+	SetTeam(Team);
+
+	SetActorHiddenInGame(false);
+	bAlwaysRelevant = true;
+
+	SetActorLocationAndRotation(WorldLocation, WorldRotation);
+	
+	Rep_Location = WorldLocation;
+	Rep_Rotation = WorldRotation;
+}
+//------------------------------------------------------------------------------
+// Functionality
+//------------------------------------------------------------------------------
+
+void AEDU_CORE_Waypoint::AddActorToWaypoint(const TObjectPtr<AEDU_CORE_SelectableEntity>& Entity)
+{ FLOW_LOG
+	if(!ListenerArray.Contains(Entity))
+	{
+		ListenerArray.AddUnique(Entity);
+		UE_LOG(FLOWLOG_CATEGORY, Warning, TEXT("%s::%hs - Adding entity: %s"), *GetClass()->GetName(), __FUNCTION__, *Entity->GetName());
+		UE_LOG(FLOWLOG_CATEGORY, Warning, TEXT("%s::%hs - ListenerArray.Num(): %d"), *GetClass()->GetName(), __FUNCTION__, ListenerArray.Num());
+	}
+}
+
+void AEDU_CORE_Waypoint::RemoveActorFromWaypoint(const TObjectPtr<AEDU_CORE_SelectableEntity>& Entity)
+{ FLOW_LOG
+	if(ListenerArray.Contains(Entity))
+	{
+		ListenerArray.RemoveSingle(Entity);
+		FLOW_LOG_WARNING("Removing Actor From Waypoint")
+
+		UE_LOG(FLOWLOG_CATEGORY, Warning, TEXT("%s::%hs - ListenerArray.Num(): %d"), *GetClass()->GetName(), __FUNCTION__, ListenerArray.Num());
+
+		if(ListenerArray.Num() == 0)
+		{
+			if(HasAuthority())
+			{
+				if(TObjectPtr<AEDU_CORE_GameMode> GameMode = Cast<AEDU_CORE_GameMode>(GetWorld()->GetAuthGameMode()))
+				{
+					// Reset the waypoint
+					GameMode->ReturnWaypointToPool(this);
+				}
+			}
+			FLOW_LOG_WARNING("Waypoint is empty, Returning to pool")
+		}
+	}
+}
+
+void AEDU_CORE_Waypoint::SetWaypointID(FGuid GUID)
+{ FLOW_LOG
+	WaypointID = GUID;
+}
+
+int32 AEDU_CORE_Waypoint::GetFormationIndex(const TObjectPtr<AEDU_CORE_SelectableEntity>& Listener) const
+{ FLOW_LOG
+	if(ListenerArray.Contains(Listener))
+	{
+		// Find the index of the actor in the array
+		int32 Index = ListenerArray.Find(Listener);
+
+		if (Index != INDEX_NONE)
+		{
+			UE_LOG(FLOWLOG_CATEGORY, Log, TEXT("Listener found in array at index: %d"), Index);
+			return Index;
+		}
+	}
+	UE_LOG(FLOWLOG_CATEGORY, Log, TEXT("Listener not found in array."));
+	return 0;
+}
+
+void AEDU_CORE_Waypoint::NotifyListeners()
+{ FLOW_LOG
+	for (int32 Index = 0; Index < ListenerArray.Num(); ++Index)
+	{
+		if(AActor* Actor = ListenerArray[Index])
+		{
+			if(IEDU_CORE_CommandInterface* Listener = Cast<IEDU_CORE_CommandInterface>(Actor))
+			{
+				Params.WaypointPtr = this;
+				Params.WaypointPosition = GetActorLocation();
+				Params.WaypointRotation = GetActorRotation();
+				Params.WaypointRightVector = GetActorRightVector();
+				Params.WaypointForwardVector = GetActorForwardVector();
+				
+				Listener->AddWaypoint(Params);
+			}
+		}
+	}
+}
+
+void AEDU_CORE_Waypoint::ResetWaypoint()
+{ FLOW_LOG
+	SetActorHiddenInGame(true);
+	Owner = nullptr;
+	ListenerArray.Reset();
+	
+	// Reset FGuid to default (all zeroes)
+	FGuid DefaultGuid;
+	WaypointID = DefaultGuid;
+
+	// Reset Params with default constructor
+	FWaypointParams NewParams;
+	Params = NewParams;
+	
+	bAlwaysRelevant = false;
+}
+
+//------------------------------------------------------------------------------
+// WaypointSelection
+//------------------------------------------------------------------------------
+
+void AEDU_CORE_Waypoint::RectangleHighlightActor()
+{
+	//Super::RectangleHighlightActor();
+}
+
+void AEDU_CORE_Waypoint::UnRectangleHighlightActor()
+{
+	//Super::UnRectangleHighlightActor();
+}
+
+//------------------------------------------------------------------------------
+// Replication
+//------------------------------------------------------------------------------
+
+void AEDU_CORE_Waypoint::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AEDU_CORE_Waypoint, Rep_Location);
+	DOREPLIFETIME(AEDU_CORE_Waypoint, Rep_Rotation);
+}
+
+void AEDU_CORE_Waypoint::OnRep_ReplicatedLocation()
+{ FLOW_LOG
+	SetActorLocation(Rep_Location);
+}
+
+void AEDU_CORE_Waypoint::OnRep_ReplicatedRotation()
+{ FLOW_LOG
+	SetActorRotation(Rep_Rotation);
+}
---
+++ Plugins/EDU_CORE/Source/EDU_CORE/Public/Entities/Waypoints/EDU_CORE_Waypoint.h
@@ -0,0 +1,130 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Framework/Data/DataTypes/EDU_CORE_DataTypes.h"
+#include "Entities/EDU_CORE_SelectableEntity.h"
+#include "EDU_CORE_Waypoint.generated.h"
+
+class AEDU_CORE_SelectableEntity;
+
+/*------------------------------------------------------------------------------
+  Abstract SUPER Class intended to be inherited from.
+--------------------------------------------------------------------------------
+  Base class for a Waypoint that can be selected and moved.
+------------------------------------------------------------------------------*/
+
+
+UCLASS(Abstract)
+class EDU_CORE_API AEDU_CORE_Waypoint : public AEDU_CORE_SelectableEntity
+{
+	GENERATED_BODY()
+
+//------------------------------------------------------------------------------
+// Construction & Init
+//------------------------------------------------------------------------------
+public:
+	AEDU_CORE_Waypoint(const FObjectInitializer& ObjectInitializer);
+
+	void InitiateWaypoint(const EEDU_CORE_Team Team, const FVector& WorldLocation, const FRotator& WorldRotation);
+
+	// Override GetLifetimeReplicatedProps
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+//------------------------------------------------------------------------------
+// Public API
+//------------------------------------------------------------------------------
+public:
+	// Adds an actor to be notified by this waypoint.
+	void AddActorToWaypoint(const TObjectPtr<AEDU_CORE_SelectableEntity>& Entity);
+
+	// removes an actor to be notified by this waypoint.
+	void RemoveActorFromWaypoint(const TObjectPtr<AEDU_CORE_SelectableEntity>& Entity);
+
+	// Sets a global, unique ID that works across instances.
+	void SetWaypointID(FGuid GUID);
+	
+	// Notifies a listener in the ListenerArray that they need to save this waypoint in their WaypointArray.
+	void NotifyListeners();
+
+	// What team does this waypoint belong to?
+	void SetTeam(const EEDU_CORE_Team NewTeam) { Params.WaypointTeam = NewTeam; };
+
+	// Resets the waypoint, so it can be returned to an ActorPool
+	void ResetWaypoint();
+
+	// Sets what kind of Waypoint this is
+	FORCEINLINE void SetWaypointType(const EEDU_CORE_WaypointType Type) { Params.WaypointType = Type; };
+
+	// Sets this Waypint to a Patrolpoint or not
+	FORCEINLINE void SetPatrolPoint(const bool bIsPatrolPoint) { Params.bPatrolPoint = bIsPatrolPoint; } ;
+	
+	// Sets what kind of Waypoint this is
+	FORCEINLINE void SetTargetLocation(const FVector& Location) { Params.TargetPosition = Location; };
+
+	// Sets what kind of Waypoint this is
+	FORCEINLINE void SetWaypointParams(const FWaypointParams& NewParams) { Params = NewParams; };
+	
+	// Getters
+	FORCEINLINE FWaypointParams GetWaypointParams()					 const	{ return Params; };
+	FORCEINLINE FGuid GetWaypointID()								 const	{ return WaypointID; };
+	
+	FORCEINLINE bool IsPatrolPoint()								 const	{ return Params.bPatrolPoint; } ;
+	FORCEINLINE FVector GetTargetPosition()							 const	{ return Params.TargetPosition; }
+	FORCEINLINE EEDU_CORE_MovementSpeed GetWaypointSpeed()			 const	{ return Params.MovementSpeed; };
+	FORCEINLINE EEDU_CORE_InfantryStance GetWaypointStance()		 const	{ return Params.InfantryStance; };
+	FORCEINLINE EEDU_CORE_WaypointType GetWaypointType()	 		 const	{ return Params.WaypointType; };
+	FORCEINLINE EEDU_CORE_AlertnessLevel GetWaypointAlertnessLevel() const	{ return Params.AlertnessLevel; };
+
+	// Return FormationIndex to a Listener
+	int32 GetFormationIndex(const TObjectPtr<AEDU_CORE_SelectableEntity>& Listener) const;
+	
+	/*-------------------------------------------------------------------------------------
+	  Highlighting and Selection are triggered by
+	  - PlayerController::CursorTrace()
+	    The playerController will trigger MouseHighlightActor on MouseHover.
+	  - HUD::DrawHUD()
+	    The playerController will trigger Highlight on all entities in Drawn Rectangle.
+	  - CameraPawn::Input_Mouse_1_Released
+	    The CameraPawn will select all entities in Drawn Rectangle on Mouse_1_Released.
+	-------------------------------------------------------------------------------------*/
+
+	virtual void RectangleHighlightActor() override;
+	virtual void UnRectangleHighlightActor() override;
+	
+//------------------------------------------------------------------------------
+// Components
+//------------------------------------------------------------------------------
+protected:
+
+	// Parameters
+	UPROPERTY(EditAnywhere, Category = "Parameters", DisplayName= "Waypoint Parameters")
+	FWaypointParams Params;
+
+	// Unique Global ID
+	UPROPERTY(VisibleAnywhere)
+	FGuid WaypointID;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<TObjectPtr<AEDU_CORE_SelectableEntity>> ListenerArray;
+
+//------------------------------------------------------------------------------
+// Replication
+//------------------------------------------------------------------------------
+public:
+	UPROPERTY(ReplicatedUsing = OnRep_ReplicatedLocation)
+	FVector_NetQuantize10 Rep_Location;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_ReplicatedRotation)
+	FRotator Rep_Rotation;
+
+protected:	
+	UFUNCTION()
+	void OnRep_ReplicatedLocation();
+
+	UFUNCTION()
+	void OnRep_ReplicatedRotation();
+};
